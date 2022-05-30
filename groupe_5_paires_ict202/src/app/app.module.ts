0f49507a3306a49f02b56115b94104b744352a86
OusmanAB <abdelkerim.ousman@facsciences-uy1.cm>
2022-05-30 20:18:46 +0100

    Mise en forme de l'application

--- groupe_5_paires_ict202/src/app/app.module.ts
+++ groupe_5_paires_ict202/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LanguagesComponent } from './home/content/languages/languages.component
 import { ProjetsPersonnelComponent } from './home/content/projets-personnel/projets-personnel.component';
 import { RouterModule } from '@angular/router';
 import { CvRoutingModule } from './home/cv-routing.module';
+import { ProfileComponent } from './home/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CvRoutingModule } from './home/cv-routing.module';
     ExperiencesComponent,
     FormationsComponent,
     LanguagesComponent,
-    ProjetsPersonnelComponent
+    ProjetsPersonnelComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
--- groupe_5_paires_ict202/src/app/home/cv-routing.module.ts
+++ groupe_5_paires_ict202/src/app/home/cv-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompetencesComponent } from './content/competences/competences.component';
 import { ExperiencesComponent } from './content/experiences/experiences.component';
+import { FormationsComponent } from './content/formations/formations.component';
 import { LanguagesComponent } from './content/languages/languages.component';
 import { ProjetsPersonnelComponent } from './content/projets-personnel/projets-personnel.component';
 import { InformationsComponent } from './informations/informations.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'home/informations', component: InformationsComponent },
   { path: 'home/experiences', component: ExperiencesComponent },
   { path: 'home/competences', component: CompetencesComponent },
+  { path: 'home/formations', component: FormationsComponent },
   { path: 'home/languages', component: LanguagesComponent },
   { path: 'home/projects', component: ProjetsPersonnelComponent }
 ]; 
