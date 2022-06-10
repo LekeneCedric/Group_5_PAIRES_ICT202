243b538422cd9513563da375032bc92351e60461
Leprincedar <94176375+Leprincedar@users.noreply.github.com>
2022-06-10 12:05:01 +0000

    Add files via upload

---
+++ groupe_5_paires_ict202/src/app/home/template 1/cv1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cv1',
+  templateUrl: './cv1.component.html',
+  styleUrls: ['./cv1.component.css']
+})
+export class Cv1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
